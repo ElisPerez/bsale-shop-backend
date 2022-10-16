5f7a9c48d777f914ae2f776e1734beb82d8abb2f
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-10-16 08:05:14 -0500

    port added

--- mysql/dbConnection.js
+++ mysql/dbConnection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
 
 const dbConnection = mysql.createPool({
-  connectionLimit: 1000000,
+  connectionLimit: 10,
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  port: process.env.PORT || 3306,
 });
 
 module.exports = { dbConnection };
