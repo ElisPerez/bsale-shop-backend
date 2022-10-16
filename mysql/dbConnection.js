81414b12e889e30930c353f9d54b5e06f798acaa
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-10-16 08:12:36 -0500

    timeout2

--- mysql/dbConnection.js
+++ mysql/dbConnection.js
@@ -2,6 +2,9 @@ const mysql = require('mysql');
 
 const dbConnection = mysql.createPool({
   connectionLimit: 10,
+  connectTimeout: 60 * 60 * 1000,
+  acquireTimeout: 60 * 60 * 1000,
+  timeout: 60 * 60 * 1000,
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
